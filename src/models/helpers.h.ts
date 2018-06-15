208f1b83c16578e3b65a2aa42a2aa17f82f8418b
Dmitry Korolev <dmitry@korolev.dk>
2018-06-15 15:16:40 +0300

    build: fix and update build process to work with travis-ci

--- src/models/helpers.h.ts
+++ src/models/helpers.h.ts
@@ -1,10 +1,4 @@
 /**
  * @private
  */
-export type Diff<T extends string, U extends string> = ({ [P in T]: P } &
-  { [P in U]: never } & { [x: string]: never })[T]
-
-/**
- * @private
- */
-export type Omit<T, K extends string> = { [P in Diff<keyof T, K>]: T[P] }
+export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
