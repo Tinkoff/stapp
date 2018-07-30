014860ae1cd7d321c60248c5d3f19434d1519dc4
Dmitry Korolev <dmitry@korolev.dk>
2018-07-30 19:25:49 +0300

    feat(core): allow to provide error type to effectCreator
    #32

--- packages/stapp/src/core/createEffect/createEffect.h.ts
+++ packages/stapp/src/core/createEffect/createEffect.h.ts
@@ -1,12 +1,18 @@
 import { Observable } from 'light-observable'
-import { EmptyEventCreator, Event, EventCreator1 } from '../createEvent/createEvent.h'
+import {
+  EmptyEventCreator,
+  Event,
+  EventCreator1
+} from '../createEvent/createEvent.h'
 
-export type EffectCreator<Payload, Result> = {
+export type EffectCreator<Payload, Result, Error = any> = {
   (payload: Payload): Observable<Event<any, any>>
   start: EventCreator1<Payload>
   success: EventCreator1<Result>
-  fail: EventCreator1<any>
+  fail: EventCreator1<Error>
   complete: EmptyEventCreator
-  use(effectFn: (payload: Payload) => Promise<Result> | Result): EffectCreator<Payload, Result>
+  use(
+    effectFn: (payload: Payload) => Promise<Result> | Result
+  ): EffectCreator<Payload, Result, Error>
   getType(): string
 }
