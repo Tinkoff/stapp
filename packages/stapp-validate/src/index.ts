5adab3b6e52a2fdc4aea95a6b0544d1706fa9ee3
Dmitry Korolev <dmitry@korolev.dk>
2018-09-13 16:19:15 +0300

    feat(validate): export revalidate event from index
    closes #102

--- packages/stapp-validate/src/index.ts
+++ packages/stapp-validate/src/index.ts
@@ -1,4 +1,10 @@
+export { revalidate } from './events'
 export { validate } from './validate'
 export { VALIDATE } from './constants'
 export { isValidatingSelector } from './selectors'
-export { ValidateConfig, ValidationFlags, ValidationRule, ValidationRules } from './validate.h'
+export {
+  ValidateConfig,
+  ValidationFlags,
+  ValidationRule,
+  ValidationRules
+} from './validate.h'
