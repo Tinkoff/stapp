2f7f3c30946f9d7ca2bec581a9836a6689466faa
m.berezin <Asbe3816>
2018-09-17 18:41:41 +0300

    add ConsumerClass to map state to component props

---
+++ packages/stapp-react/src/context/ConsumerClass.ts
@@ -0,0 +1,49 @@
+import shallowEqual from 'fbjs/lib/shallowEqual'
+import { Subscription } from 'light-observable'
+import { Component } from 'react'
+import { Stapp } from 'stapp'
+import { ConsumerProps } from '..'
+import { renderComponent } from '../helpers/renderComponent'
+
+export class ConsumerClass<State, Api> extends Component<
+  { app: Stapp<State, Api> } & ConsumerProps<State, Api, any>
+> {
+  subscription?: Subscription = undefined
+
+  state = {
+    appState: null
+  }
+
+  componentWillMount() {
+    const app = this.props.app
+
+    this.subscription = app.subscribe(this.setAppState.bind(this))
+  }
+
+  componentWillUnmount() {
+    this.subscription && this.subscription.unsubscribe()
+  }
+
+  shouldComponentUpdate(nextProps: any, nextState: { appState: any }) {
+    return !shallowEqual(this.state.appState, nextState.appState)
+  }
+
+  setAppState(state: State) {
+    const app = this.props.app
+
+    this.setState({ appState: this.props.map!(state, app.api) })
+  }
+
+  render() {
+    const { app, ...props } = this.props
+    const appState = this.state.appState
+
+    return renderComponent({
+      name: 'Consumer',
+      renderProps: props,
+      result: appState,
+      api: app.api,
+      app
+    })
+  }
+}
