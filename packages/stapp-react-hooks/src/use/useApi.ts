5916eb7e5b032df6401466ae4855d5ab4b32daba
Dmitry Korolev <dmitry@korolev.dk>
2019-02-14 12:14:06 +0300

    docs(stapp-react-hooks): update docs

--- packages/stapp-react-hooks/src/use/useApi.ts
+++ packages/stapp-react-hooks/src/use/useApi.ts
@@ -1,8 +1,9 @@
 import { useContext } from 'react'
+import { Stapp, StappApi } from 'stapp'
 import { StappContext } from 'stapp-react/lib/shared/StappContext'
 import { STAPP_REACT_HOOKS } from '../helpers/constants'
 
-export const useApi = () => {
+export const useApi = <T extends Stapp<any, any>>(): StappApi<T> => {
   const app = useContext(StappContext)
 
   /* istanbul ignore next */
